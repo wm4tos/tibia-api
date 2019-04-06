5ee6b48d863d99a5254e74d6556372e574185a71
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-06 16:38:05 -0300

    implementar método que valida se o usuário existe ou não

--- src/services/character.service.js
+++ src/services/character.service.js
@@ -2,13 +2,26 @@ const { GetDomFromURL } = require('../utils/dom.utils');
 
 const Character = require('../models/Character');
 
-const GetCharacterInfos = async (playerName) => {
+const GetCharacterInfos = (playerName) => {
   if (!playerName) throw new Error('Player name is required');
 
-  const url = `https://secure.tibia.com/community/?subtopic=characters&name=${playerName}`;
-  const dom = await GetDomFromURL(url);
-  
-  return new Character(dom).allCharacterInformation;
+  return new Promise(async (resolve, reject) => {
+    try {
+      const url = `https://secure.tibia.com/community/?subtopic=characters&name=${playerName}`;
+      const dom = await GetDomFromURL(url);
+
+      const character = new Character(dom);
+      
+      if(character.PlayerDoesntExists()) {
+        reject(new Error("Player doesn't exists"));
+      }
+
+      resolve(character.allCharacterInformation);
+    } catch (err) {
+      reject(err);
+    };
+  });
+
 }
 
 module.exports = {
