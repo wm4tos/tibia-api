78da86aee87a5d86de553483ec70bfa46977acf0
Wesley Matos <wmatos@3xsolutions.com.br>
2019-04-06 16:21:36 -0300

    ciar getters de informações do personagem

--- src/models/Character.js
+++ src/models/Character.js
@@ -6,11 +6,54 @@ class Character {
   }
 
   get name() {
-    const playerNameSelector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2)`
-
-    const name = GetTextContent(this._dom, playerNameSelector)
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(2) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get sex() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(3) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get vocation() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(4) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get level() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(5) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get achievementPoints() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(6) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get world() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(7) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get residence() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(8) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get lastLogin() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(9) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
+  get accountStatus() {
+    const selector = `#characters > div.Border_2 > div > div > table:nth-child(1) > tbody > tr:nth-child(10) > td:nth-child(2)`
+    return GetTextContent(this._dom, selector)
+  }
  
-    return name
+  get allCharacterInformation(){
+    return {
+      name: this.name,
+      sex: this.sex,
+      vocation: this.vocation,
+      level: this.level,
+      achievementPoints: this.achievementPoints,
+      world: this.world,
+      residence: this.residence,
+      lastLogin: this.lastLogin,
+      accountStatus: this.accountStatus,
+    }
   }
 }
 
